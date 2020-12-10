372852af0a9d9492e4e8e564b1666800fe31f614
Alex <alexmiddleton@outlook.com>
2020-12-09 21:16:00 -0500

    Styled admin Users page.  TO DO: Fix drop-down value on same page so that it submits correctly.

--- app/src/pages/users/users.js
+++ app/src/pages/users/users.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Paper from "@material-ui/core/Paper";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
 
 import "./users.css";
 
@@ -19,39 +25,69 @@ const Users = () => {
   const submitLevel = () => {
     axios.put(`/api/users`, adjustment);
   };
-
-  console.log(adjustment);
+  console.log("Users", users);
+  console.log("Roles", roles);
+  console.log("Adjustment", adjustment);
   return (
     <div>
       <h1>Users:</h1>
-      {users &&
-        users.map(user => (
-          <div key={user.id}>
-            <h2>{user.username}</h2>
-            <div>
-              Current level: <b>{user.level}</b>
+      <div className="user-info-wrapper">
+        {users &&
+          users.map((user, index) => (
+            <Paper
+              className="user-info-paper"
+              elevation={3}
+              square
+              key={user.id}
+            >
+              <h2>{user.username}</h2>
+              <hr style={{ width: "70%", textAlign: "center" }} />
               <br />
-              <select
-                name="level"
-                id="level"
-                onChange={e =>
-                  setAdjustment({
-                    ...adjustment,
-                    id: user.id,
-                    level: e.target.value
-                  })
-                }
-              >
-                {roles &&
-                  roles.map(role => (
-                    <option key={role.level}>{role.level}</option>
-                  ))}
-              </select>
-              <button onClick={submitLevel}>Change</button>
-            </div>
-            <hr />
-          </div>
-        ))}
+              <div>
+                Current permission level: <b>{user.level}</b>
+                <br />
+                <br />
+                <br />
+                <FormControl variant="outlined" style={{ minWidth: "35%" }}>
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    Level
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    name="level"
+                    id="level"
+                    value={user[index].level}
+                    label="Level"
+                    onChange={e =>
+                      setAdjustment({
+                        ...adjustment,
+                        id: user.id,
+                        level: e.target.value
+                      })
+                    }
+                  >
+                    {roles &&
+                      roles.map((role, index) => (
+                        <MenuItem key={index}>{role.level}</MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+                <br />
+                <br />
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  onClick={submitLevel}
+                >
+                  Change
+                </Button>
+              </div>
+              <br />
+            </Paper>
+          ))}
+      </div>
     </div>
   );
 };
